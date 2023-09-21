149d966c4d1b9edd0fd5a2e94f622cd322bbc4b5
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-21 16:21:23 -0700

    update monty_funcc_02

--- monty_funcc_02.c
+++ monty_funcc_02.c
@@ -7,6 +7,12 @@ int funcc_instru(stack_t **header,char *data, unsigned int number, FILE *file)
 {
     instruction_t opst[] = {
                 {"push", funcc_push},
+                {"pall", funcc_pall},
+                {"swap", funcc_swap},
+                {"add", funcc_add},
+                {"pint", funcc_pint},
+                {"pop", funcc_pop},
+                {"nop", funcc_nop},
                 {NULL, NULL}
                 };
     unsigned int i = 0;
@@ -33,5 +39,55 @@ int funcc_instru(stack_t **header,char *data, unsigned int number, FILE *file)
         funcc_free_stack(*header);
         exit(EXIT_FAILURE); }
     return (1);
+}
+
+
+void funcc_pall(stack_t **header, unsigned int numi)
+{
+    stack_t *hea;
+    (void)numi;
+
+
+    hea = *header;
+    if (!hea)
+        return;
+    while (hea)
+    {
+        printf("%d\n", hea->n);
+        hea = hea->next;
+    }
+}
+
+
+void funcc_pint(stack_t **header, unsigned int numi)
+{
+    if (!*header)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", numi);
+        fclose(global_var.file);
+        free(global_var.conte);
+        funcc_free_stack(*header);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*header)->n);
+}
+
+
+void funcc_pop(stack_t **header, unsigned int numi)
+{
+    stack_t *hea;
+
+
+    if (!*header)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", numi);
+        fclose(global_var.file);
+        free(global_var.conte);
+        funcc_free_stack(*header);
+        exit(EXIT_FAILURE);
+    }
+    hea = *header;
+    *header = hea->next;
+    free(hea);
 }
 
