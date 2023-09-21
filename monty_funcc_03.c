5834c7cb41b10dd0c040d37997c0fb4c4b0657a4
ahmedyak1 <ahmedyakine1@gmail.com>
2023-09-21 16:40:41 -0700

    update monty_funcc_03.c

--- monty_funcc_03.c
+++ monty_funcc_03.c
@@ -74,3 +74,58 @@ void funcc_nop(stack_t **header, unsigned int numi)
     (void) numi;
 }
 
+
+void funcc_sub(stack_t **header, unsigned int numi)
+{
+    stack_t *tmp;
+    int k;
+    int no;
+
+
+    tmp = *header;
+    for (no = 0; tmp != NULL; no++)
+        tmp = tmp->next;
+    if (no < 2)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", numi);
+        fclose(global_var.file);
+        free(global_var.conte);
+        funcc_free_stack(*header);
+        exit(EXIT_FAILURE);
+    }
+    tmp = *header;
+    k = tmp->next->n - tmp->n;
+    tmp->next->n = k;
+    *header = tmp->next;
+    free(tmp);
+}
+
+
+void funcc_mul(stack_t **header, unsigned int numi)
+{
+    stack_t *hea;
+    int k = 0;
+    int tmp;
+
+
+    hea = *header;
+    while (hea)
+    {
+        k++;
+        hea = hea->next;
+    }
+    if (k < 2)
+    {
+        fprintf(stderr, "L%d: can't mul, stack too short\n", numi);
+        fclose(global_var.file);
+        free(global_var.conte);
+        funcc_free_stack(*header);
+        exit(EXIT_FAILURE);
+    }
+    hea = *header;
+    tmp = hea->next->n * hea->n;
+    hea->next->n = tmp;
+    *header = hea->next;
+    free(hea);
+}
+
